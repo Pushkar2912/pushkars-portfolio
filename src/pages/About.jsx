1efe09801119809f3b4e5ddf7fa77ec566490232
= <=>
2024-03-31 14:08:09 +0530

    modified-experience

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -23,11 +23,10 @@ const About = () => {
                     <p className='text-xl font-medium text-white'>Tata Consultancy Services, Pune — Software Engineer</p>
                     <p className='text-xs text-white'>Oct 2021 - Jan 2024</p>
                     <p className='text-slate-400 text-sm'>
-                        - Troubleshoot and resolve CAD software issues for users<br />
-                        - Install and configure CAD software <br />
-                        - Develop and maintain documentation, such as user guides and FAQs <br />
-                        - Stay up-to-date on the latest CAD software releases and updates <br />
-                        - Collaborate with other IT professionals to resolve complex issues <br />
+                        - Developed features like searching, translation engine etc. using different Web Technologies like HTML5 and CSS3, used Javascript for adding interactivity to the web pages.<br />
+                        - Led the translation team to provide internationalization in the product. <br />
+                        - Contributed in various internal projects which used React, Redux, Mantine UI etc. This helped me strengthen my understanding of UI Technologies. <br />
+                        - Technologies Used - HTML5, CSS3, Javascript, React, NodeJS, SQL <br />
                     </p>
                 </div>
             </div>
